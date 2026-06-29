b054e9c93d21e1960b8289fd8019ffdac4fa83ed
carlosnogueira-creator <carlos.nogueira@alunos.ifsuldeminas.edu.br>
2026-06-29 12:17:46 -0300

    Add Cliente class to represent system clients
    This class represents a client in the system with various attributes and their corresponding getters and setters.

---
+++ techfix-manager/src/main/java/br/com/techfix/model/Cliente.java
@@ -0,0 +1,91 @@
+package br.com.techfix.model;
+
+/**
+ * Cliente do sistema.
+ */
+public class Cliente {
+
+    private int id;
+    private String nome;
+    private String cpfCnpj;
+    private String telefone;
+    private String email;
+    private String endereco;
+    private boolean status;
+
+    public Cliente(int id,
+                    String nome,
+                    String cpfCnpj,
+                    String telefone,
+                    String email,
+                    String endereco,
+                    boolean status) {
+        this.id = id;
+        this.nome = nome;
+        this.cpfCnpj = cpfCnpj;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+        this.status = status;
+    }
+
+    public Cliente() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
+
